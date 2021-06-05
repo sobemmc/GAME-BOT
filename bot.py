a96618a7079229aec2aa8371a4d96a8db54df912
TEAM SOBI <64724470+sobemmc@users.noreply.github.com>
2021-06-05 20:32:23 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,21 +3,53 @@ import asyncio
 import random
 import os
 
-countG = 0
-client = discord.Client()
-players = {}
-queues= {}
-musiclist=[]
-mCount=1
-searchYoutube={}
-searchYoutubeHref={}
 
-def check_queue(id):
-    if queues[id]!=[]:
-        player = queues[id].pop(0)
-        players[id] = player
-        del musiclist[0]
-        player.start()
+client = discord.Client()
+
+
+tierScore = {
+    'default' : 0,
+    'iron' : 1,
+    'bronze' : 2,
+    'silver' : 3,
+    'gold' : 4,
+    'platinum' : 5,
+    'diamond' : 6,
+    'master' : 7,
+    'grandmaster' : 8,
+    'challenger' : 9
+}
+def tierCompare(solorank,flexrank):
+    if tierScore[solorank] > tierScore[flexrank]:
+        return 0
+    elif tierScore[solorank] < tierScore[flexrank]:
+        return 1
+    else:
+        return 2
+warnings.filterwarnings(action='ignore')
+bot = commands.Bot(command_prefix='!')
+
+opggsummonersearch = 'https://www.op.gg/summoner/userName='
+bottoken = ''
+
+'''
+Simple Introduction about asyncio
+asyncio : Asynchronous I/O. It is a module for asynchronous programming and allows CPU operations to be handled in parallel with I/O.
+async def (func name)(parameters): -> This type of asynchronous function or method is called Native Co-Rutine.
+- await : you can use await keyword only in Native Co-Rutine
+async def add(a,b):
+    print("add {0} + {1}".format(a,b))
+    await asyncio.sleep(1.0)
+    return a + b
+async def print_add(a,b):
+    result = await add(a,b)
+    print("print_add : {0} + {1} = {2}".format(a,b,result))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(print_add(1,2))
+loop.close()
+'''
+
+
         
 @client.event
 async def on_ready():
@@ -31,88 +63,179 @@ async def on_ready():
 @client.event
 async def on_message(message):
     
-    
-     if message.content.startswith("$날씨"):
-        learn = message.content.split(" ")
-        location = learn[1]
-        enc_location = urllib.parse.quote(location+'날씨')
-        hdr = {'User-Agent': 'Mozilla/5.0'}
-        url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
-        print(url)
-        req = Request(url, headers=hdr)
-        html = urllib.request.urlopen(req)
-        bsObj = bs4.BeautifulSoup(html, "html.parser")
-        todayBase = bsObj.find('div', {'class': 'main_info'})
 
-        todayTemp1 = todayBase.find('span', {'class': 'todaytemp'})
-        todayTemp = todayTemp1.text.strip()  # 온도
-        print(todayTemp)
+    if message.content.startswith("$롤전적"):
+        try:
+            if len(message.content.split(" ")) == 1:
+                embed = discord.Embed(title="소환사 이름이 입력되지 않았습니다!", description="", color=0x5CD1E5)
+                embed.add_field(name="Summoner name not entered",
+                                value="To use command $롤전적 : $롤전적 (Summoner Nickname)", inline=False)
+                embed.set_footer(text='SOBI',
+                                 icon_url='https://media.discordapp.net/attachments/795588047381987375/845185855068569640/SOBI.png?width=658&height=702')
+                await message.channel.send("Error : Incorrect command usage ", embed=embed)
+            else:
+                playerNickname = ''.join((message.content).split(' ')[1:])
+                # Open URL
+                checkURLBool = urlopen(opggsummonersearch + quote(playerNickname))
+                bs = BeautifulSoup(checkURLBool, 'html.parser')
 
-        todayValueBase = todayBase.find('ul', {'class': 'info_list'})
-        todayValue2 = todayValueBase.find('p', {'class': 'cast_txt'})
-        todayValue = todayValue2.text.strip()  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
-        print(todayValue)
+                # 자유랭크 언랭은 뒤에 '?image=q_auto&v=1'표현이없다
 
-        todayFeelingTemp1 = todayValueBase.find('span', {'class': 'sensible'})
-        todayFeelingTemp = todayFeelingTemp1.text.strip()  # 체감온도
-        print(todayFeelingTemp)
+                # Patch Note 20200503에서
+                # Medal = bs.find('div', {'class': 'ContentWrap tabItems'}) 이렇게 바꾸었었습니다.
+                # PC의 설정된 환경 혹은 OS플랫폼에 따라서 ContentWrap tabItems의 띄어쓰기가 인식이
 
-        todayMiseaMongi1 = bsObj.find('div', {'class': 'sub_info'})
-        todayMiseaMongi2 = todayMiseaMongi1.find('div', {'class': 'detail_box'})
-        todayMiseaMongi3 = todayMiseaMongi2.find('dd')
-        todayMiseaMongi = todayMiseaMongi3.text  # 미세먼지
-        print(todayMiseaMongi)
+                Medal = bs.find('div', {'class': 'SideContent'})
+                RankMedal = Medal.findAll('img', {'src': re.compile('\/\/[a-z]*\-[A-Za-z]*\.[A-Za-z]*\.[A-Za-z]*\/[A-Za-z]*\/[A-Za-z]*\/[a-z0-9_]*\.png')})
+                # Variable RankMedal's index 0 : Solo Rank
+                # Variable RankMedal's index 1 : Flexible 5v5 rank
 
-        tomorrowBase = bsObj.find('div', {'class': 'table_info weekly _weeklyWeather'})
-        tomorrowTemp1 = tomorrowBase.find('li', {'class': 'date_info'})
-        tomorrowTemp2 = tomorrowTemp1.find('dl')
-        tomorrowTemp3 = tomorrowTemp2.find('dd')
-        tomorrowTemp = tomorrowTemp3.text.strip()  # 오늘 오전,오후온도
-        print(tomorrowTemp)
+                # for mostUsedChampion
+                mostUsedChampion = bs.find('div', {'class': 'ChampionName'})
+                mostUsedChampionKDA = bs.find('span', {'class': 'KDA'})
 
-        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
-        tomorrowMoring1 = tomorrowAreaBase.find('div', {'class': 'main_info morning_box'})
-        tomorrowMoring2 = tomorrowMoring1.find('span', {'class': 'todaytemp'})
-        tomorrowMoring = tomorrowMoring2.text.strip()  # 내일 오전 온도
-        print(tomorrowMoring)
+                # 솔랭, 자랭 둘다 배치가 안되어있는경우 -> 사용된 챔피언 자체가 없다. 즉 모스트 챔피언 메뉴를 넣을 필요가 없다.
 
-        tomorrowValue1 = tomorrowMoring1.find('div', {'class': 'info_data'})
-        tomorrowValue = tomorrowValue1.text.strip()  # 내일 오전 날씨상태, 미세먼지 상태
-        print(tomorrowValue)
+                # Scrape Summoner's Rank information
+                # [Solorank,Solorank Tier]
+                solorank_Types_and_Tier_Info = deleteTags(bs.findAll('div', {'class': {'RankType', 'TierRank'}}))
+                # [Solorank LeaguePoint, Solorank W, Solorank L, Solorank Winratio]
+                solorank_Point_and_winratio = deleteTags(
+                    bs.findAll('span', {'class': {'LeaguePoints', 'wins', 'losses', 'winratio'}}))
+                # [Flex 5:5 Rank,Flexrank Tier,Flextier leaguepoint + W/L,Flextier win ratio]
+                flexrank_Types_and_Tier_Info = deleteTags(bs.findAll('div', {
+                    'class': {'sub-tier__rank-type', 'sub-tier__rank-tier', 'sub-tier__league-point',
+                              'sub-tier__gray-text'}}))
+                # ['Flextier W/L]
+                flexrank_Point_and_winratio = deleteTags(bs.findAll('span', {'class': {'sub-tier__gray-text'}}))
 
-        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
-        tomorrowAllFind = tomorrowAreaBase.find_all('div', {'class': 'main_info morning_box'})
-        tomorrowAfter1 = tomorrowAllFind[1]
-        tomorrowAfter2 = tomorrowAfter1.find('p', {'class': 'info_temperature'})
-        tomorrowAfter3 = tomorrowAfter2.find('span', {'class': 'todaytemp'})
-        tomorrowAfterTemp = tomorrowAfter3.text.strip()  # 내일 오후 온도
-        print(tomorrowAfterTemp)
+                # embed.set_imag()는 하나만 들어갈수 있다.
 
-        tomorrowAfterValue1 = tomorrowAfter1.find('div', {'class': 'info_data'})
-        tomorrowAfterValue = tomorrowAfterValue1.text.strip()
+                # 솔랭, 자랭 둘다 배치 안되어있는 경우 -> 모스트 챔피언 출력 X
+                if len(solorank_Point_and_winratio) == 0 and len(flexrank_Point_and_winratio) == 0:
+                    embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
+                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                                    inline=False)
+                    embed.add_field(name="Ranked Solo : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name="Flex 5:5 Rank : Unranked", value="Unranked", inline=False)
+                    embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
+                    embed.set_footer(text='SOBI',
+                                     icon_url='https://media.discordapp.net/attachments/795588047381987375/845185855068569640/SOBI.png?width=658&height=702')
+                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
 
-        print(tomorrowAfterValue)  # 내일 오후 날씨상태,미세먼지
+                # 솔로랭크 기록이 없는경우
+                elif len(solorank_Point_and_winratio) == 0:
 
-        embed = discord.Embed(
-            title=learn[1]+ ' 날씨 정보',
-            description=learn[1]+ '날씨 정보입니다.',
-            colour=discord.Colour.gold()
-        )
-        embed.add_field(name='현재온도', value=todayTemp+'˚', inline=False)  # 현재온도
-        embed.add_field(name='체감온도', value=todayFeelingTemp, inline=False)  # 체감온도
-        embed.add_field(name='현재상태', value=todayValue, inline=False)  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
-        embed.add_field(name='현재 미세먼지 상태', value=todayMiseaMongi, inline=False)  # 오늘 미세먼지
-        embed.add_field(name='오늘 오전/오후 날씨', value=tomorrowTemp, inline=False)  # 오늘날씨 # color=discord.Color.blue()
-        embed.add_field(name='**----------------------------------**',value='**----------------------------------**', inline=False)  # 구분선
-        embed.add_field(name='내일 오전온도', value=tomorrowMoring+'˚', inline=False)  # 내일오전날씨
-        embed.add_field(name='내일 오전날씨상태, 미세먼지 상태', value=tomorrowValue, inline=False)  # 내일오전 날씨상태
-        embed.add_field(name='내일 오후온도', value=tomorrowAfterTemp + '˚', inline=False)  # 내일오후날씨
-        embed.add_field(name='내일 오후날씨상태, 미세먼지 상태', value=tomorrowAfterValue, inline=False)  # 내일오후 날씨상태
+                    # most Used Champion Information : Champion Name, KDA, Win Rate
+                    mostUsedChampion = bs.find('div', {'class': 'ChampionName'})
+                    mostUsedChampion = mostUsedChampion.a.text.strip()
+                    mostUsedChampionKDA = bs.find('span', {'class': 'KDA'})
+                    mostUsedChampionKDA = mostUsedChampionKDA.text.split(':')[0]
+                    mostUsedChampionWinRate = bs.find('div', {'class': "Played"})
+                    mostUsedChampionWinRate = mostUsedChampionWinRate.div.text.strip()
 
+                    FlexRankTier = flexrank_Types_and_Tier_Info[0] + ' : ' + flexrank_Types_and_Tier_Info[1]
+                    FlexRankPointAndWinRatio = flexrank_Types_and_Tier_Info[2] + " /" + flexrank_Types_and_Tier_Info[-1]
+                    embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
+                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                                    inline=False)
+                    embed.add_field(name="Ranked Solo : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name=FlexRankTier, value=FlexRankPointAndWinRatio, inline=False)
+                    embed.add_field(name="Most Used Champion : " + mostUsedChampion,
+                                    value="KDA : " + mostUsedChampionKDA + " / " + " WinRate : " + mostUsedChampionWinRate,
+                                    inline=False)
+                    embed.set_thumbnail(url='https:' + RankMedal[1]['src'])
+                    embed.set_footer(text='Service provided by Hoplin.',
+                                     icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
+                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
 
+                # 자유랭크 기록이 없는경우
+                elif len(flexrank_Point_and_winratio) == 0:
 
-        await client.send_message(message.channel,embed=embed)
-      
+                    # most Used Champion Information : Champion Name, KDA, Win Rate
+                    mostUsedChampion = bs.find('div', {'class': 'ChampionName'})
+                    mostUsedChampion = mostUsedChampion.a.text.strip()
+                    mostUsedChampionKDA = bs.find('span', {'class': 'KDA'})
+                    mostUsedChampionKDA = mostUsedChampionKDA.text.split(':')[0]
+                    mostUsedChampionWinRate = bs.find('div', {'class': "Played"})
+                    mostUsedChampionWinRate = mostUsedChampionWinRate.div.text.strip()
+
+                    SoloRankTier = solorank_Types_and_Tier_Info[0] + ' : ' + solorank_Types_and_Tier_Info[1]
+                    SoloRankPointAndWinRatio = solorank_Point_and_winratio[0] + "/ " + solorank_Point_and_winratio[
+                        1] + " " + solorank_Point_and_winratio[2] + " /" + solorank_Point_and_winratio[3]
+                    embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
+                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                                    inline=False)
+                    embed.add_field(name=SoloRankTier, value=SoloRankPointAndWinRatio, inline=False)
+                    embed.add_field(name="Flex 5:5 Rank : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name="Most Used Champion : " + mostUsedChampion,
+                                    value="KDA : " + mostUsedChampionKDA + " / " + "WinRate : " + mostUsedChampionWinRate,
+                                    inline=False)
+                    embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
+                    embed.set_footer(text='SOBI',
+                                     icon_url='https://media.discordapp.net/attachments/795588047381987375/845185855068569640/SOBI.png?width=658&height=702')
+                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
+                # 두가지 유형의 랭크 모두 완료된사람
+                else:
+                    # 더 높은 티어를 thumbnail에 안착
+                    solorankmedal = RankMedal[0]['src'].split('/')[-1].split('?')[0].split('.')[0].split('_')
+                    flexrankmedal = RankMedal[1]['src'].split('/')[-1].split('?')[0].split('.')[0].split('_')
+
+                    # Make State
+                    SoloRankTier = solorank_Types_and_Tier_Info[0] + ' : ' + solorank_Types_and_Tier_Info[1]
+                    SoloRankPointAndWinRatio = solorank_Point_and_winratio[0] + "/ " + solorank_Point_and_winratio[
+                        1] + " " + solorank_Point_and_winratio[2] + " /" + solorank_Point_and_winratio[3]
+                    FlexRankTier = flexrank_Types_and_Tier_Info[0] + ' : ' + flexrank_Types_and_Tier_Info[1]
+                    FlexRankPointAndWinRatio = flexrank_Types_and_Tier_Info[2] + " /" + flexrank_Types_and_Tier_Info[-1]
+
+                    # most Used Champion Information : Champion Name, KDA, Win Rate
+                    mostUsedChampion = bs.find('div', {'class': 'ChampionName'})
+                    mostUsedChampion = mostUsedChampion.a.text.strip()
+                    mostUsedChampionKDA = bs.find('span', {'class': 'KDA'})
+                    mostUsedChampionKDA = mostUsedChampionKDA.text.split(':')[0]
+                    mostUsedChampionWinRate = bs.find('div', {'class': "Played"})
+                    mostUsedChampionWinRate = mostUsedChampionWinRate.div.text.strip()
+
+                    cmpTier = tierCompare(solorankmedal[0], flexrankmedal[0])
+                    embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
+                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                                    inline=False)
+                    embed.add_field(name=SoloRankTier, value=SoloRankPointAndWinRatio, inline=False)
+                    embed.add_field(name=FlexRankTier, value=FlexRankPointAndWinRatio, inline=False)
+                    embed.add_field(name="Most Used Champion : " + mostUsedChampion,
+                                    value="KDA : " + mostUsedChampionKDA + " / " + " WinRate : " + mostUsedChampionWinRate,
+                                    inline=False)
+                    if cmpTier == 0:
+                        embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
+                    elif cmpTier == 1:
+                        embed.set_thumbnail(url='https:' + RankMedal[1]['src'])
+                    else:
+                        if solorankmedal[1] > flexrankmedal[1]:
+                            embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
+                        elif solorankmedal[1] < flexrankmedal[1]:
+                            embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
+                        else:
+                            embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
+
+                    embed.set_footer(text='SOBI',
+                                     icon_url='https://media.discordapp.net/attachments/795588047381987375/845185855068569640/SOBI.png?width=658&height=702')
+                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
+        except HTTPError as e:
+            embed = discord.Embed(title="소환사 전적검색 실패", description="", color=0x5CD1E5)
+            embed.add_field(name="", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
+            await message.channel.send("Wrong Summoner Nickname")
+
+        except UnicodeEncodeError as e:
+            embed = discord.Embed(title="소환사 전적검색 실패", description="", color=0x5CD1E5)
+            embed.add_field(name="???", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
+            await message.channel.send("Wrong Summoner Nickname", embed=embed)
+
+        except AttributeError as e:
+            embed = discord.Embed(title="존재하지 않는 소환사", description="", color=0x5CD1E5)
+            embed.add_field(name="해당 닉네임의 소환사가 존재하지 않습니다.", value="소환사 이름을 확인해주세요", inline=False)
+            embed.set_footer(text='SOBI',
+                             icon_url='https://media.discordapp.net/attachments/795588047381987375/845185855068569640/SOBI.png?width=658&height=702')
+            await message.channel.send("Error : Non existing Summoner ", embed=embed)      
     
 
         
