e231c86678c620591e68aa6e2f175a017f437a26
TEAM SOBI <64724470+sobemmc@users.noreply.github.com>
2021-06-05 20:07:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,39 +4,203 @@ import asyncio
 import random
 import os
 
+countG = 0
+client = discord.Client()
+players = {}
+queues= {}
+musiclist=[]
+mCount=1
+searchYoutube={}
+searchYoutubeHref={}
 
-bot = commands.Bot(command_prefix='$')
-
-@bot.event
+def check_queue(id):
+    if queues[id]!=[]:
+        player = queues[id].pop(0)
+        players[id] = player
+        del musiclist[0]
+        player.start()
+        
+@client.event
 async def on_ready():
-    print('logged in as \nname: {}\n  id: {}'.format(bot.user.name, bot.user.id))
-    print('='*80)
 
-    await bot.change_presence(game=discord.Game(name='$팀짜기 [한 팀당 사람 수]', type=1))
+    print(client.user.name)
+    print('성공적으로 봇이 시작되었습니다.')
+    game = discord.Game('LOL')
+    await client.change_presence(status=discord.Status.online, activity=game)
 
-@bot.command(pass_context=True)
-async def 팀짜기(ctx, count):
-    voice_channel = ctx.message.author.voice.voice_channel
+    
+@client.event
+async def on_message(message):
+    
+    
+     if message.content.startswith("$날씨"):
+        learn = message.content.split(" ")
+        location = learn[1]
+        enc_location = urllib.parse.quote(location+'날씨')
+        hdr = {'User-Agent': 'Mozilla/5.0'}
+        url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=' + enc_location
+        print(url)
+        req = Request(url, headers=hdr)
+        html = urllib.request.urlopen(req)
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        todayBase = bsObj.find('div', {'class': 'main_info'})
 
-    members = voice_channel.voice_members
-    member_names = []
-    for member in members:
-        member_names.append(member.mention)
-    random.shuffle(member_names) #DevSkim: ignore DS148264 until 2021-07-05 
+        todayTemp1 = todayBase.find('span', {'class': 'todaytemp'})
+        todayTemp = todayTemp1.text.strip()  # 온도
+        print(todayTemp)
 
-    team = []
+        todayValueBase = todayBase.find('ul', {'class': 'info_list'})
+        todayValue2 = todayValueBase.find('p', {'class': 'cast_txt'})
+        todayValue = todayValue2.text.strip()  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
+        print(todayValue)
 
-    for i in range(0, int(len(member_names)/int(count))):
-        temp = []
-        for c in range(0, int(count)):
-            temp.append(member_names.pop())
-        team.append(temp)
+        todayFeelingTemp1 = todayValueBase.find('span', {'class': 'sensible'})
+        todayFeelingTemp = todayFeelingTemp1.text.strip()  # 체감온도
+        print(todayFeelingTemp)
 
-    if member_names:
-        team.append(member_names)
+        todayMiseaMongi1 = bsObj.find('div', {'class': 'sub_info'})
+        todayMiseaMongi2 = todayMiseaMongi1.find('div', {'class': 'detail_box'})
+        todayMiseaMongi3 = todayMiseaMongi2.find('dd')
+        todayMiseaMongi = todayMiseaMongi3.text  # 미세먼지
+        print(todayMiseaMongi)
 
-    for index in range(0, len(team)):
-        await bot.say('{} team : {}'.format(index+1, team[index]))
+        tomorrowBase = bsObj.find('div', {'class': 'table_info weekly _weeklyWeather'})
+        tomorrowTemp1 = tomorrowBase.find('li', {'class': 'date_info'})
+        tomorrowTemp2 = tomorrowTemp1.find('dl')
+        tomorrowTemp3 = tomorrowTemp2.find('dd')
+        tomorrowTemp = tomorrowTemp3.text.strip()  # 오늘 오전,오후온도
+        print(tomorrowTemp)
+
+        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
+        tomorrowMoring1 = tomorrowAreaBase.find('div', {'class': 'main_info morning_box'})
+        tomorrowMoring2 = tomorrowMoring1.find('span', {'class': 'todaytemp'})
+        tomorrowMoring = tomorrowMoring2.text.strip()  # 내일 오전 온도
+        print(tomorrowMoring)
+
+        tomorrowValue1 = tomorrowMoring1.find('div', {'class': 'info_data'})
+        tomorrowValue = tomorrowValue1.text.strip()  # 내일 오전 날씨상태, 미세먼지 상태
+        print(tomorrowValue)
+
+        tomorrowAreaBase = bsObj.find('div', {'class': 'tomorrow_area'})
+        tomorrowAllFind = tomorrowAreaBase.find_all('div', {'class': 'main_info morning_box'})
+        tomorrowAfter1 = tomorrowAllFind[1]
+        tomorrowAfter2 = tomorrowAfter1.find('p', {'class': 'info_temperature'})
+        tomorrowAfter3 = tomorrowAfter2.find('span', {'class': 'todaytemp'})
+        tomorrowAfterTemp = tomorrowAfter3.text.strip()  # 내일 오후 온도
+        print(tomorrowAfterTemp)
+
+        tomorrowAfterValue1 = tomorrowAfter1.find('div', {'class': 'info_data'})
+        tomorrowAfterValue = tomorrowAfterValue1.text.strip()
+
+        print(tomorrowAfterValue)  # 내일 오후 날씨상태,미세먼지
+
+        embed = discord.Embed(
+            title=learn[1]+ ' 날씨 정보',
+            description=learn[1]+ '날씨 정보입니다.',
+            colour=discord.Colour.gold()
+        )
+        embed.add_field(name='현재온도', value=todayTemp+'˚', inline=False)  # 현재온도
+        embed.add_field(name='체감온도', value=todayFeelingTemp, inline=False)  # 체감온도
+        embed.add_field(name='현재상태', value=todayValue, inline=False)  # 밝음,어제보다 ?도 높거나 낮음을 나타내줌
+        embed.add_field(name='현재 미세먼지 상태', value=todayMiseaMongi, inline=False)  # 오늘 미세먼지
+        embed.add_field(name='오늘 오전/오후 날씨', value=tomorrowTemp, inline=False)  # 오늘날씨 # color=discord.Color.blue()
+        embed.add_field(name='**----------------------------------**',value='**----------------------------------**', inline=False)  # 구분선
+        embed.add_field(name='내일 오전온도', value=tomorrowMoring+'˚', inline=False)  # 내일오전날씨
+        embed.add_field(name='내일 오전날씨상태, 미세먼지 상태', value=tomorrowValue, inline=False)  # 내일오전 날씨상태
+        embed.add_field(name='내일 오후온도', value=tomorrowAfterTemp + '˚', inline=False)  # 내일오후날씨
+        embed.add_field(name='내일 오후날씨상태, 미세먼지 상태', value=tomorrowAfterValue, inline=False)  # 내일오후 날씨상태
 
 
-bot.run(os.environ['token'])
+
+        await client.send_message(message.channel,embed=embed)
+      
+    
+    
+    if message.content.startswith("$롤"):
+        learn = message.content.split(" ")
+        location = learn[1]
+        enc_location = urllib.parse.quote(location)
+
+        url = "http://www.op.gg/summoner/userName=" + enc_location
+        html = urllib.request.urlopen(url)
+
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        rank1 = bsObj.find("div", {"class": "TierRankInfo"})
+        rank2 = rank1.find("div", {"class": "TierRank"})
+        rank3 = rank2.find("span", {"class": "tierRank"})
+        rank4 = rank3.text  # 티어표시 (브론즈1,2,3,4,5 등등)
+        print(rank4)
+        if rank4 != 'Unranked':
+          jumsu1 = rank1.find("div", {"class": "TierInfo"})
+          jumsu2 = jumsu1.find("span", {"class": "LeaguePoints"})
+          jumsu3 = jumsu2.text
+          jumsu4 = jumsu3.strip()#점수표시 (11LP등등)
+          print(jumsu4)
+
+          winlose1 = jumsu1.find("span", {"class": "WinLose"})
+          winlose2 = winlose1.find("span", {"class": "wins"})
+          winlose2_1 = winlose1.find("span", {"class": "losses"})
+          winlose2_2 = winlose1.find("span", {"class": "winratio"})
+
+          winlose2txt = winlose2.text
+          winlose2_1txt = winlose2_1.text
+          winlose2_2txt = winlose2_2.text #승,패,승률 나타냄  200W 150L Win Ratio 55% 등등
+
+          print(winlose2txt + " " + winlose2_1txt + " " + winlose2_2txt)
+
+        channel = message.channel
+        embed = discord.Embed(
+            title='롤 정보',
+            description='롤 정보입니다.',
+            colour=discord.Colour.green()
+        )
+        if rank4=='Unranked':
+            embed.add_field(name='당신의 티어', value=rank4, inline=False)
+            embed.add_field(name='-당신은 언랭-', value="언랭은 더이상의 정보는 제공하지 않습니다.", inline=False)
+            await client.send_message(channel, embed=embed)
+        else:
+         embed.add_field(name='당신의 티어', value=rank4, inline=False)
+         embed.add_field(name='당신의 LP(점수)', value=jumsu4, inline=False)
+         embed.add_field(name='당신의 승,패 정보', value=winlose2txt+" "+winlose2_1txt, inline=False)
+         embed.add_field(name='당신의 승률', value=winlose2_2txt, inline=False)
+         await client.send_message(channel, embed=embed)    
+        
+
+    if message.content.startswith('$이모티콘'):
+
+        emoji = [" ꒰⑅ᵕ༚ᵕ꒱ ", " ꒰◍ˊ◡ˋ꒱ ", " ⁽⁽◝꒰ ˙ ꒳ ˙ ꒱◜⁾⁾ ", " ༼ つ ◕_◕ ༽つ ", " ⋌༼ •̀ ⌂ •́ ༽⋋ ",
+                 " ( ･ิᴥ･ิ) ", " •ө• ", " ค^•ﻌ•^ค ", " つ╹㉦╹)つ ", " ◕ܫ◕ ", " ᶘ ͡°ᴥ͡°ᶅ ", " ( ؕؔʘ̥̥̥̥ ه ؔؕʘ̥̥̥̥ ) ",
+                 " ( •́ ̯•̀ ) ",
+                 " •̀.̫•́✧ ", " '͡•_'͡• ", " (΄◞ิ౪◟ิ‵) ", " ˵¯͒ བ¯͒˵ ", " ͡° ͜ʖ ͡° ", " ͡~ ͜ʖ ͡° ", " (づ｡◕‿‿◕｡)づ ",
+                 " ´_ゝ` ", " ٩(͡◕_͡◕ ", " ⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄ ", " ٩(͡ï_͡ï☂ ", " ௐ ", " (´･ʖ̫･`) ", " ε⌯(ง ˙ω˙)ว ",
+                 " (っ˘ڡ˘ς) ", "●▅▇█▇▆▅▄▇", "╋╋◀", "︻╦̵̵̿╤──", "ー═┻┳︻▄", "︻╦̵̵͇̿̿̿̿══╤─",
+                 " ጿ ኈ ቼ ዽ ጿ ኈ ቼ ዽ ጿ ", "∑◙█▇▆▅▄▃▂", " ♋♉♋ ", " (๑╹ω╹๑) ", " (╯°□°）╯︵ ┻━┻ ",
+                 " (///▽///) ", " σ(oдolll) ", " 【o´ﾟ□ﾟ`o】 ", " ＼(^o^)／ ", " (◕‿‿◕｡) ", " ･ᴥ･ ", " ꈍ﹃ꈍ "
+                                                                                                 " ˃̣̣̣̣̣̣︿˂̣̣̣̣̣̣ ",
+                 " ( ◍•㉦•◍ ) ", " (｡ì_í｡) ", " (╭•̀ﮧ •́╮) ", " ଘ(੭*ˊᵕˋ)੭ ", " ´_ゝ` ", " (~˘▾˘)~ "] # 이모티콘 배열입니다.
+
+        randomNum = random.randrange(0, len(emoji)) # 0 ~ 이모티콘 배열 크기 중 랜덤숫자를 지정합니다.
+        print("랜덤수 값 :" + str(randomNum))
+        print(emoji[randomNum])
+        await client.send_message(message.channel, embed=discord.Embed(description=emoji[randomNum])) # 랜덤 이모티콘을 메시지로 출력합니다.
+
+        
+    if message.content.startswith('$주사위'):
+
+        randomNum = random.randrange(1, 7) # 1~6까지 랜덤수
+        print(randomNum)
+        if randomNum == 1:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: '+ ':one:'))
+        if randomNum == 2:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':two:'))
+        if randomNum ==3:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':three:'))
+        if randomNum ==4:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':four:'))
+        if randomNum ==5:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':five:'))
+        if randomNum ==6:
+            await client.send_message(message.channel, embed=discord.Embed(description=':game_die: ' + ':six: '))
+        
+        
+client.run(os.environ['token'])
